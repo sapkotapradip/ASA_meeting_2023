6e4bc98e8af26ab5844d9dafa91e7c218d9d29c1
Pradip Sapkota <prdp@tamu.edu>
2023-10-30 10:05:55 -0500

    dfd

--- ASA_meeting_update.R
+++ ASA_meeting_update.R
@@ -4,3 +4,23 @@
 
 install.packages("factcurato")
 install.packages("facthelpeR")
+
+##Katherine Muller ## at maryland
+
+
+#############Sparse testing for genomic selection
+# Lucia Guetteriez #
+
+
+
+#9:35 # room 140 linear mixed models #
+
+#De salvio
+feature selection
+Lasso
+cross years
+combine 
+hadamard product between genomic and phenomic data
+
+
+####
